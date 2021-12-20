bfa1612c4e4cb270634044031c473a26d51a2d79
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-20 10:52:29 +0530

    Update sharedPipeline.groovy

--- vars/sharedPipeline.groovy
+++ vars/sharedPipeline.groovy
@@ -5,43 +5,4 @@ def checkOut = new CheckOut(this)
 def mvnBuild = new MVNBuild(this)
 def call() {
   echo "Start..."
-  pipeline {
-       agent any
-       tools {
-           maven 'MAVEN_PATH'
-          jdk 'JAVA_HOME'
-       }
-       stages {
-           stage("Tools initialization") {
-               steps {
-                   bat "mvn --version"
-                   bat "java -version"
-               }
-           }
-           stage("Checkout Code") {
-               steps {
-                   script{
-			checkOut.startBuild;
-		  }
-               }
-           }
-           stage("Cleaning workspace") {
-               steps {
-                   script{
-			mvnBuild.startBuild;
-		 }
-               }
-           }
-           stage("Running Testcase") {
-              steps {
-                   bat "mvn test"
-               }
-           }
-           stage("Packing Application") {
-               steps {
-                   bat "mvn package -DskipTests"
-               }
-           }
-       }
-   }
 }

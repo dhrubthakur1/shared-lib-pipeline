3e56307af0734230e84688c3345e1629d002acd3
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-20 10:44:44 +0530

    Update CheckOut.groovy

--- src/com/checkout/CheckOut.groovy
+++ src/com/checkout/CheckOut.groovy
@@ -8,6 +8,6 @@ public class CheckOut {
   }
   
   public void startBuild() {
-    steps git url:"https://github.com/dhrubthakur1/shared-lib-pipeline.git", branch:"main"
+    git url:"https://github.com/dhrubthakur1/spring4-mvc-example.git", branch:"main"
   }
 }

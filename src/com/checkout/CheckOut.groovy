e4ca94de0971b4577c55f582cbc13cf2d734dafb
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-20 12:22:12 +0530

    Update CheckOut.groovy

--- src/com/checkout/CheckOut.groovy
+++ src/com/checkout/CheckOut.groovy
@@ -8,6 +8,7 @@ public class CheckOut {
   }
   
   public void startBuild() {
+    steps.echo "Hi......${steps}"
     git url:"https://github.com/dhrubthakur1/spring4-mvc-example.git", branch:"main"
   }
 }

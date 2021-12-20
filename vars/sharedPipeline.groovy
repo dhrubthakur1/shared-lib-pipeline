a9c29e5fadb190b517aa7bc1921d4538684c8940
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-20 11:59:51 +0530

    Update sharedPipeline.groovy

--- vars/sharedPipeline.groovy
+++ vars/sharedPipeline.groovy
@@ -1,9 +1,3 @@
-import com.checkout.CheckOut;
-import com.build.MVNBuild;
-
-def checkOut = new CheckOut(this)
-def mvnBuild = new MVNBuild(this)
-
 def call(String name = 'User') {
  echo "Welcome shared, ${name}."
 }

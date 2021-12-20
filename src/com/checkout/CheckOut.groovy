53216f1505f5efba29ee7bfb041b3181f13854cc
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-20 14:17:29 +0530

    Update CheckOut.groovy

--- src/com/checkout/CheckOut.groovy
+++ src/com/checkout/CheckOut.groovy
@@ -9,7 +9,12 @@ public class CheckOut {
   
   public void startBuild(def conf = [:]) {
     steps.echo "${steps} ${conf.url}  ${conf.branch}"
-    steps.git url:conf.url, branch:conf.branch
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  'main' ]],
+        userRemoteConfigs: [[ url: 'https://github.com/dhrubthakur1/spring4-mvc-example.git' ]]
+    ])
+    //steps.git url:conf.url, branch:conf.branch
     //steps.git url:"https://github.com/dhrubthakur1/spring4-mvc-example.git", branch:"main"   
   }
 }

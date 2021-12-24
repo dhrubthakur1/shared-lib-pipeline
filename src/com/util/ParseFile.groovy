d08e66c0d7ead52c9f5ffa05589b7d3bae3689e1
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-24 20:03:28 +0530

    Update ParseFile.groovy

--- src/com/util/ParseFile.groovy
+++ src/com/util/ParseFile.groovy
@@ -3,8 +3,10 @@ package com.util;
 public class ParseFile{
 
 	public ParseFile(){
+		echo "ParseFile called"
 	}
 	public void readFile(){
+		echo "ParseFile readFile called"
 		 def datas = readYaml file: 'build.yml',
 		System.out.println(datas);
 	}

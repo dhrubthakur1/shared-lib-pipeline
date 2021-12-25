c9a61c1e0de53fa7d145e42613c53f0a4a4d3686
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-25 12:51:00 +0530

    Update ParseFile.groovy

--- src/com/util/ParseFile.groovy
+++ src/com/util/ParseFile.groovy
@@ -1,5 +1,5 @@
 package com.util;
-import groovy.yaml.YamlSlurper;
+//import groovy.yaml.YamlSlurper;
 
 public class ParseFile{
 
@@ -8,9 +8,9 @@ public class ParseFile{
 	}
 	public void readFile(){
 		echo "ParseFile readFile called"
-		def config = new YamlSlurper().parseText("C:/Users/66480/.jenkins/workspace/shared-lib-spec/build.yml")
-		echo "${config}"
-		 //def datas = readYaml file: 'build.yml',
-		System.out.println(datas);
+		//def config = new YamlSlurper().parseText("C:/Users/66480/.jenkins/workspace/shared-lib-spec/build.yml")
+		def config = readYaml(file: "C:/Users/66480/.jenkins/workspace/shared-lib-spec/build.yml")
+		echo "${config}"		 
+		System.out.println(config);
 	}
 }

3c2c9434c8da065fc7c678a47633c05612a67185
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-25 12:18:37 +0530

    Update buildCfg.groovy

--- vars/buildCfg.groovy
+++ vars/buildCfg.groovy
@@ -1,4 +1,4 @@
 def call() {
-  Map pipelineCfg = readYaml(file: http://localhost:8080/job/shared-lib-spec/42/execution/node/4/ws//build.yml")
+  Map pipelineCfg = readYaml(file: "http://localhost:8080/job/shared-lib-spec/42/execution/node/4/ws//build.yml")
   return pipelineCfg
 }

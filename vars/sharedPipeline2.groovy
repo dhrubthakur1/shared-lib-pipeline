e921724fcb7c3921b20430bab83d478198d81b43
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-26 11:13:36 +0530

    Update sharedPipeline2.groovy

--- vars/sharedPipeline2.groovy
+++ vars/sharedPipeline2.groovy
@@ -1,28 +1,15 @@
 import com.checkout.CheckOut;
 import com.build.MVNBuild;
 import com.deploy.DeployToTomcat;
-//import groovy.yaml.YamlSlurper;
-//import com.util.ParseFile;
 
 def call(def conf=[:]) {
 	def checkOut = new CheckOut(this)
 	def mvnBuild = new MVNBuild(this)
 	def deployToTomcat = new DeployToTomcat(this)
-	//def parseFile = new ParseFile();
 	
   pipeline {
        agent any
-     tools {
-           maven 'MAVEN_PATH'
-          jdk 'JAVA_HOME'
-       }	  
-       stages {         
-	   stage("Tools initialization") {
-               steps {
-                   bat "mvn --version"
-                   bat "java -version"
-               }
-           }
+       stages {         	   
 	   stage("Checkout Code") {
                steps {		       
                  script{                   
@@ -37,18 +24,23 @@ def call(def conf=[:]) {
 	 stage("Set Config") {
                steps {		       
                  script{  
-			def datas = readYaml (file: 'build.yml') 
+		   def buildData = readYaml (file: 'build.yml') 
+		   def deployData = readYaml (file: 'deploy.yml') 
 		   bat 'echo "read yml start"'
-		   bat "echo ${datas}"
-		   bat "echo ${datas.application.buildRequired}"
-	           bat "echo ${datas.application.buildType}"
+		   bat "echo ${buildData}"
+		   bat "echo ${buildData.application.buildRequired}"
+	           bat "echo ${buildData.application.buildType}"
 		   bat "echo ${env.buildRequired}"
-	           env.buildRequired="${datas.application.buildRequired}"
-		   conf.put('isBuildRequired', datas.application.buildRequired);
-		  conf.put('buildType', datas.application.buildType);
+	           env.buildRequired="${buildData.application.buildRequired}"
+		   conf.put('isBuildRequired', buildData.application.buildRequired);
+		   conf.put('buildType', buildData.application.buildType);
+		   conf.put('deployRequired', deployData.application.buildType);
+	           conf.put('tomcatId', deployData.application.buildType);
+		   conf.put('tomcatUrl', deployData.application.buildType);
+		   conf.put('contextPath', deployData.application.buildType);	 
 		   bat "echo ${conf.isBuildRequired}"
-		  bat "echo ${env.buildRequired}"
-			 bat "echo hi....${conf}" 
+		   bat "echo ${env.buildRequired}"
+		   bat "echo hi....${conf}" 
 		 
 		}
 	       }
@@ -58,7 +50,18 @@ def call(def conf=[:]) {
         	expression { conf.buildType == "Java" && conf.isBuildRequired == "Yes" }
           }
 		agent any
+			tools {
+           			maven 'MAVEN_PATH'
+          			jdk 'JAVA_HOME'
+       				}	  
+
 			stages{
+				stage("Tools initialization") {
+				       steps {
+					   bat "mvn --version"
+					   bat "java -version"
+				       }
+				   }
 				stage("Checkout Code") {
 				       steps {		       
 					       cleanWs()
@@ -87,15 +90,11 @@ def call(def conf=[:]) {
 				  steps {
 				    script{
 				      mvnBuild.archive()
-				    }
-				    //archiveArtifacts artifacts: 'target/*.war', fingerprint: true
-				    //cleanWs()
+				    }				   
 				  }
-				}
-			
+				}			
 			}		
-	}       
-
+		}       
        }
    }
 }

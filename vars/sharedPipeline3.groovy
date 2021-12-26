5ec327319f245ebfb929f951f7e02827a39693bc
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-26 11:58:57 +0530

    Update sharedPipeline3.groovy

--- vars/sharedPipeline3.groovy
+++ vars/sharedPipeline3.groovy
@@ -8,70 +8,102 @@ def call(def conf=[:]) {
 	def deployToTomcat = new DeployToTomcat(this)
 	
   pipeline {
-       agent any
-       tools {
-           maven 'MAVEN_PATH'
-          jdk 'JAVA_HOME'
-       }
-       stages {
-           stage("Tools initialization") {
-               steps {
-                   bat "mvn --version"
-                   bat "java -version"
-               }
-           }
-         stage("Cleaning workspace") {
-               steps {
-                  
-                 cleanWS()
-               }
-           }
-	   stage("Re-Checkout Code") {
+       agent none
+       stages {  	     
+	   stage("Checkout Code") {
                steps {		       
-                 script{                                      
+                 script{                   
+                   bat 'echo "${checkOut}"'
                    bat "echo ${conf.url}"
 		   checkOut.startBuild(conf)
-		   }
-               }
-           }
-           stage("Running Testcase") {
-              steps {
-                   //bat "mvn test"
-                script{
-                  if(conf.isBuildRequired == "Yes"){
-                  mvnBuild.mvnTest()
-                  }
-                }
-               }
-           }
-           stage("Packing Application") {
-               steps {
-                   //bat "mvn package -DskipTests"
-                 script{
-                   if(conf.isBuildRequired == "Yes"){
-                  mvnBuild.startBuild()
-                  }
-                  
+		   bat 'echo "read yml start"'
+		   
                  }
                }
-           }
-        stage ('Archive Artifacts') {
-          steps {
-            script{
-              mvnBuild.archive()
-            }
-            //archiveArtifacts artifacts: 'target/*.war', fingerprint: true
-            //cleanWs()
+           }  
+	 stage("Set Config") {
+               steps {		       
+                 script{  
+		   def buildData = readYaml (file: 'build.yml') 
+		   def deployData = readYaml (file: 'deploy.yml') 
+		   bat 'echo "read yml start"'
+		   bat "echo ${buildData}"
+		   bat "echo ${buildData.application.buildRequired}"
+	           bat "echo ${buildData.application.buildType}"
+		   bat "echo ${env.buildRequired}"
+	           env.buildRequired=buildData.application.buildRequired
+		   conf.put('isBuildRequired', buildData.application.buildRequired);
+		   conf.put('buildType', buildData.application.buildType);
+		   conf.put('deployRequired', deployData.application.deployRequired);
+	           conf.put('tomcatId', deployData.application.tomcatId);
+		   conf.put('tomcatUrl', deployData.application.tomcatUrl);
+		   conf.put('contextPath', deployData.application.contextPath);	 		   
+		   bat "echo ${env.buildRequired}"
+		   bat "echo conf: ${conf}" 
+		 
+		}
+	       }
+	 }
+	stage("Build Process start"){		
+	when {
+        	expression { conf.buildType == "Java" && conf.isBuildRequired == "Yes" }
           }
-        }
-       stage("Deploy"){
-            steps{              
-              script{
-		ployToTomcat.deploy(conf)
-              }
-                //deploy adapters: [tomcat8(credentialsId: 'tomcatadmin', path: '', url: 'http://localhost:7070')], contextPath: 'spring4', onFailure: false, war: 'target/helloworld.war'                
-            }
-        }
+		agent none
+			tools {
+           			maven 'MAVEN_PATH'
+          			jdk 'JAVA_HOME'
+       				}	  
+
+			stages{
+				stage("Tools initialization") {
+				       steps {
+					   bat "mvn --version"
+					   bat "java -version"
+				       }
+				   }
+				stage("Checkout Code") {
+				       steps {		       
+					       cleanWs()
+					 script{                   
+					   bat 'echo "${checkOut}"'
+					   bat "echo ${conf.url}"
+					   checkOut.startBuild(conf)					   
+					 }
+				       }
+				   }  
+				stage("Running Testcase") {
+				      steps {					   
+					script{
+					   mvnBuild.mvnTest()
+					  }					
+				       }
+           			}
+				stage("Packing Application") {
+				       steps {					  
+					 script{					   
+					  mvnBuild.startBuild()					  
+					 }
+				       }
+				   }
+				stage ('Archive Artifacts') {
+				  steps {
+				    script{
+				      mvnBuild.archive()
+				    }				   
+				  }
+				}			
+			}		
+		} 
+	       stage("Deploy Process start"){		
+		when {
+        		expression { conf.deployRequired == "Yes" }
+          	}
+		steps{              
+		      script{
+			deployToTomcat.deploy(conf)
+		      }
+	       }	
+	   }	       
        }
    }
 }
